8cf2bcca5373c2938db40a1a012235d89542f0fd
Alex J Burke <alex@alexjeffburke.com>
2020-01-04 17:35:00 +0100

    Rewrite remaining "to have values satisfying" tests.

--- test/assertions/Map.to-have-values-satisfying.spec.js
+++ test/assertions/Map.to-have-values-satisfying.spec.js
@@ -1,29 +1,71 @@
 /* global expect */
-describe('to have values satiasying assertion', function() {
-  it('should have values satisfying an object', function() {
+describe('to have values satisfying assertion', function() {
+  it('should pass with an assertion', function() {
+    expect(function() {
+      expect(new Map([[[], null]]), 'to have values satisfying', 'to be null');
+    }, 'not to error');
+  });
+
+  it('should pass with an expect.it', function() {
+    expect(function() {
+      expect(
+        new Map([[[], null]]),
+        'to have values satisfying',
+        expect.it('to be null')
+      );
+    }, 'not to error');
+  });
+
+  it('should pass with an object value and satisfy semantics', function() {
     expect(function() {
       expect(new Map([[[], { foo: null }]]), 'to have values satisfying', {
-        foo: expect.it('to be null')
+        foo: expect.it('to be falsy')
       });
     }, 'not to error');
   });
 
-  it('should output a diff when failing "to have values satisfying"', function() {
+  it('fails if the given Map is empty', () => {
+    expect(
+      function() {
+        expect(
+          new Map(),
+          'to have values satisfying',
+          expect.it(value => {
+            expect(value, 'to equal', '0');
+          })
+        );
+      },
+      'to throw an error satisfying',
+      'to equal snapshot',
+      expect.unindent`
+        expected Map([]) to have values satisfying
+        expect.it(value => {
+          expect(value, 'to equal', '0');
+        })
+          expected Map([]) not to be empty
+      `
+    );
+  });
+
+  it('should render a diff when failing "to have values satisfying"', function() {
     expect(
       function() {
         expect(new Map([[[], { foo: null }]]), 'to have values satisfying', {
           foo: expect.it('to be a number')
         });
       },
-      'to error',
-      'expected Map([ [[], { foo: null }] ])\n' +
-        "to have values satisfying { foo: expect.it('to be a number') }\n" +
-        '\n' +
-        'Map([\n' +
-        '  [[], {\n' +
-        '    foo: null // should be a number\n' +
-        '  }]\n' +
-        '])'
+      'to throw an error satisfying',
+      'to equal snapshot',
+      expect.unindent`
+        expected Map([ [[], { foo: null }] ])
+        to have values satisfying { foo: expect.it('to be a number') }
+
+        Map([
+          [[], {
+            foo: null // should be a number
+          }]
+        ])
+      `
     );
   });
 
