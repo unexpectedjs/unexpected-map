a3b8e0bd3e240a43b5699f34fb2eeae039dd8738
Christopher Hiller <boneskull@boneskull.com>
2020-11-03 15:12:40 -0800

    add "to have size" assertion

---
+++ test/assertions/Map.to-have-size.spec.js
@@ -0,0 +1,32 @@
+/* global expect */
+describe('size assertion', () => {
+  it('asserts a Map has a specific size', () => {
+    expect(new Map([['foo', 'bar']]), 'to have size', 1);
+  });
+
+  it('asserts a Map does not have a specific size', () => {
+    expect(new Map([['foo', 'bar']]), 'not to have size', 2);
+  });
+
+  it('should throw when Map is of unexpected size', () => {
+    expect(
+      () => {
+        expect(new Map([['foo', 'bar']]), 'to have size', 2);
+      },
+      'to throw an error satisfying',
+      'to equal snapshot',
+      "expected Map([ ['foo', 'bar'] ]) to have size 2"
+    );
+  });
+
+  it('should throw when Map size is unexpectedly equal', () => {
+    expect(
+      () => {
+        expect(new Map([['foo', 'bar']]), 'not to have size', 1);
+      },
+      'to throw an error satisfying',
+      'to equal snapshot',
+      "expected Map([ ['foo', 'bar'] ]) not to have size 1"
+    );
+  });
+});
