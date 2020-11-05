01792d951589261eeef240fb1b91e9ad08b28baa
Alex J Burke <alex@alexjeffburke.com>
2020-11-05 13:00:59 +0100

    Align with other plugins by making use of prettier --check.

--- .eslintrc.js
+++ .eslintrc.js
@@ -17,14 +17,4 @@ const config = {
   ]
 };
 
-if (process.stdin.isTTY) {
-  // Enable plugin-prettier when running in a terminal. Allows us to have
-  // eslint verify prettier formatting, while not being bothered by it in our
-  // editors.
-  config.plugins = config.plugins || [];
-  config.plugins.push('prettier');
-  config.rules = config.rules || {};
-  config.rules['prettier/prettier'] = 'error';
-}
-
 module.exports = config;
